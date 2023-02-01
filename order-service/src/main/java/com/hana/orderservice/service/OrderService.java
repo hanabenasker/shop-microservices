082eabf490b779cf89106575ce0f1acfe55c7782
hanabenasker <hanabenasker@insat.u-carthage.tn>
2023-02-01 13:47:52 +0100

    adding discovery server using netflix eureka

--- order-service/src/main/java/com/hana/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/hana/orderservice/service/OrderService.java
@@ -23,7 +23,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
 
@@ -42,8 +42,8 @@ public class OrderService {
                 .toList();
         // Call inventory service and place order if product is in stock
 
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8084/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
